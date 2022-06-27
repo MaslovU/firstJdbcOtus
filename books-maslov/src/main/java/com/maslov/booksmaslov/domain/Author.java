619cd4e9e7cebbea2cabee0ae25e045a455257d9
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2022-06-27 21:11:05 +0500

    added domain Author, Genre

--- books-maslov/src/main/java/com/maslov/booksmaslov/domain/Author.java
+++ books-maslov/src/main/java/com/maslov/booksmaslov/domain/Author.java
@@ -1,4 +1,11 @@
 package com.maslov.booksmaslov.domain;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
 public class Author {
+    private final int id;
+    private final String name;
 }
--- books-maslov/src/main/java/com/maslov/booksmaslov/domain/Genre.java
+++ books-maslov/src/main/java/com/maslov/booksmaslov/domain/Genre.java
@@ -1,4 +1,11 @@
 package com.maslov.booksmaslov.domain;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
 public class Genre {
+    private final int id;
+    private final String name;
 }
