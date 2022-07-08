153d0945af888c3a95f24359ffde807c77ce3541
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2022-07-08 12:39:09 +0500

    added @Fetch(FetchMode.SUBSELECT)

--- books-maslov/src/main/java/com/maslov/booksmaslov/domain/Book.java
+++ books-maslov/src/main/java/com/maslov/booksmaslov/domain/Book.java
@@ -2,12 +2,15 @@ package com.maslov.booksmaslov.domain;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @RequiredArgsConstructor
 @Data
 public class Book implements Comparable<Book> {
     private final Integer id;
     private final String name;
+    @Fetch(FetchMode.SUBSELECT)
     private final String author;
     private final String yearOfPublishing;
     private final String genre;
