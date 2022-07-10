9aae9c369ef2cbea725224f5964796e3666d9abc
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2022-07-10 12:22:10 +0500

    N+1 fixed

--- books-maslov/src/main/java/com/maslov/booksmaslov/sql/SQLConstants.java
+++ books-maslov/src/main/java/com/maslov/booksmaslov/sql/SQLConstants.java
@@ -4,11 +4,33 @@ public class SQLConstants {
     private SQLConstants() {
     }
 
-    public static final String GET_ALL_BOOKS = "select id, name, author_id, year_of_publishing, genre_id from book";
-    public static final String SELECT_BOOK_BY_ID = "select id, name, author_id, year_of_publishing, " +
-            "genre_id from book where id =:id";
-    public static final String SELECT_BOOK_BY_NAME = "select id, name, author_id, year_of_publishing, " +
-            "genre_id from book where name =:name";
+    public static final String GET_ALL_BOOKS = " select b.id as id,\n" +
+            " b.name as name,\n" +
+            " a.name as author,\n" +
+            " b.year_of_publishing as year_of_publishing,\n" +
+            " g.name as genre\n" +
+            " from book b\n" +
+            " join author a on cast(b.author_id as int) = a.id\n" +
+            " join genre g on cast(b.genre_id as int) = g.id;";
+    public static final String SELECT_BOOK_BY_ID = " select b.id as id,\n" +
+            " b.name as name,\n" +
+            " a.name as author,\n" +
+            " b.year_of_publishing as year_of_publishing,\n" +
+            " g.name as genre\n" +
+            " from book b\n" +
+            " join author a on cast(b.author_id as int) = a.id\n" +
+            " join genre g on cast(b.genre_id as int) = g.id\n" +
+            " where b.id=:id";
+
+    public static final String SELECT_BOOK_BY_NAME = " select b.id as id,\n" +
+            " b.name as name,\n" +
+            " a.name as author,\n" +
+            " b.year_of_publishing as year_of_publishing,\n" +
+            " g.name as genre\n" +
+            " from book b\n" +
+            " join author a on cast(b.author_id as int) = a.id\n" +
+            " join genre g on cast(b.genre_id as int) = g.id\n" +
+            " where b.name=:name";
     public static final String UPDATE_BOOK_BY_ID = "update book set id=:id, name=:name, author_id=:author_id, " +
             "year_of_publishing=:year_of_publishing, genre_id=:genre_id where id=:id";
     public static final String INSERT_INTO_BOOK = "insert into book (id, name, author_id, year_of_publishing, genre_id)" +
