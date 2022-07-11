4bbfa7b290560a9491c07915d382da0d53083121
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2022-07-11 23:26:10 +0500

    polish

--- books-maslov/src/main/java/com/maslov/booksmaslov/exception/NoAuthorException.java
+++ books-maslov/src/main/java/com/maslov/booksmaslov/exception/NoAuthorException.java
@@ -1,8 +1,5 @@
 package com.maslov.booksmaslov.exception;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class NoAuthorException extends RuntimeException {
     public NoAuthorException(String message) {
         super(message);
