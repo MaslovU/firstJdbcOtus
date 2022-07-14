c09f6c050b0308e3d42b3b52d2c61bab7324552f
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2022-07-14 14:38:35 +0500

    refactoring for createBook

--- books-maslov/src/main/java/com/maslov/booksmaslov/repository/impl/BookDaoImpl.java
+++ books-maslov/src/main/java/com/maslov/booksmaslov/repository/impl/BookDaoImpl.java
@@ -7,25 +7,19 @@ import com.maslov.booksmaslov.repository.BookDao;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityGraph;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static com.maslov.booksmaslov.sql.SQLConstants.DELETE_BOOK;
 import static com.maslov.booksmaslov.sql.SQLConstants.GET_ALL_BOOKS;
 import static com.maslov.booksmaslov.sql.SQLConstants.SELECT_BOOK_BY_NAME;
-import static com.maslov.booksmaslov.sql.SQLConstants.UPDATE_AUTHORS_BY_ID;
-import static com.maslov.booksmaslov.sql.SQLConstants.UPDATE_BOOK_BY_ID;
 
 @Component
 @RequiredArgsConstructor
@@ -58,18 +52,15 @@ public class BookDaoImpl implements BookDao {
 
     @Override
     public Book createBook(Book book) {
-        if (book.getId() == 0) {
-            em.persist(book);
-            return book;
-        }
-        return em.merge(book);
+        em.persist(book);
+        return book;
     }
 
     @Override
     public Optional<Book> updateBook(long id, String name, String author, long authorId) {
         Book bookFromDB = em.find(Book.class, id);
         List<Author> authors = Stream.of(author.split(","))
-                .map(s ->new Author(authorId, s))
+                .map(s -> new Author(authorId, s))
                 .collect(Collectors.toList());
         Book book = new Book(id, name, bookFromDB.getGenre(), bookFromDB.getYear(), authors, bookFromDB.getListOfComment());
         em.merge(book);
