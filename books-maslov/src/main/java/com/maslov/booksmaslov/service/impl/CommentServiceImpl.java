ad4a19ffe87adf5e8c342e3610c9cc1298f1e836
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2022-07-22 11:27:27 +0500

    fixed LIE

--- books-maslov/src/main/java/com/maslov/booksmaslov/service/impl/CommentServiceImpl.java
+++ books-maslov/src/main/java/com/maslov/booksmaslov/service/impl/CommentServiceImpl.java
@@ -1,18 +1,16 @@
 package com.maslov.booksmaslov.service.impl;
 
-import com.maslov.booksmaslov.domain.Book;
 import com.maslov.booksmaslov.domain.Comment;
-import com.maslov.booksmaslov.exception.MaslovBookException;
 import com.maslov.booksmaslov.repository.BookDao;
 import com.maslov.booksmaslov.repository.CommentDao;
 import com.maslov.booksmaslov.service.CommentService;
 import com.maslov.booksmaslov.service.ScannerHelper;
+import com.maslov.booksmaslov.service.ServiceHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Service
@@ -23,15 +21,19 @@ public class CommentServiceImpl implements CommentService {
     private final BookDao bookDao;
     private final CommentDao commentDao;
 
-    public CommentServiceImpl(ScannerHelper helper, BookDao bookDao, CommentDao commentDao) {
+    private final ServiceHelper serviceHelper;
+
+    public CommentServiceImpl(ScannerHelper helper, BookDao bookDao, CommentDao commentDao, ServiceHelper serviceHelper) {
         this.helper = helper;
         this.bookDao = bookDao;
         this.commentDao = commentDao;
+        this.serviceHelper = serviceHelper;
     }
 
+    @Transactional
     @Override
     public Set<Comment> getAllCommentsForBook() {
-        long idOfBook = getIdForBook();
+        long idOfBook = serviceHelper.getIdForBook();
         return commentDao.findCommentsForBookById(idOfBook);
     }
 
@@ -49,44 +51,20 @@ public class CommentServiceImpl implements CommentService {
     @Transactional
     @Override
     public Set<Comment> updateComment() {
-        int idForBook = getIdForBook();
-        int idComment = getCommentId(idForBook);
+        int idForBook = serviceHelper.getIdForBook();
+        int idComment = serviceHelper.getCommentId(idForBook);
         System.out.println("Enter correct comment");
         String newComment = helper.getFromUser();
         commentDao.updateComment(new Comment(idComment, newComment));
-        return bookDao.getBookById(idForBook).get().getListOfComments();
+        return bookDao.getBookById(idForBook).orElseThrow().getListOfComments();
     }
 
     @Override
     public Set<Comment> deleteComment() {
-        int idForBook = getIdForBook();
-        int idForComment = getCommentId(idForBook);
+        int idForBook = serviceHelper.getIdForBook();
+        int idForComment = serviceHelper.getCommentId(idForBook);
         Comment comment = commentDao.getCommentById(idForComment);
         commentDao.deleteComment(comment);
-        Set<Comment> commentList = bookDao.getBookById(idForBook).get().getListOfComments();
-        return commentList;
-    }
-
-    private int getIdForBook() {
-        System.out.println("Enter name for book");
-        String nameOfBook = helper.getFromUser();
-        List<Book> listOfBooks = bookDao.getBooksByName(nameOfBook);
-        if (listOfBooks.isEmpty()) {
-            for (Book b : listOfBooks) {
-                System.out.println(b);
-            }
-            System.out.println("Find id your book and enter it");
-            return helper.getIdFromUser();
-        } else {
-            return 0;
-        }
-    }
-
-    private int getCommentId(int idForBook) {
-        System.out.println("Choose and enter id of comment");
-        for (Comment c : bookDao.getBookById(idForBook).get().getListOfComments()) {
-            System.out.println(c);
-        }
-        return helper.getIdFromUser();
+        return bookDao.getBookById(idForBook).orElseThrow().getListOfComments();
     }
 }
